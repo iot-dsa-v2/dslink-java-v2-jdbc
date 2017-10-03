a838593c0c9b7cec4a1644336ed448a4efbdeead
JurisPuchin <juris.puchin@gmail.com>
2017-10-03 11:51:32 -0700

    Remove connections

--- src/main/java/org/iot/dsa/dslink/java/v2/jdbc/DBConnectionNode.java
+++ src/main/java/org/iot/dsa/dslink/java/v2/jdbc/DBConnectionNode.java
@@ -1,6 +1,9 @@
 package org.iot.dsa.dslink.java.v2.jdbc;
 
 import org.iot.dsa.node.*;
+import org.iot.dsa.node.action.ActionInvocation;
+import org.iot.dsa.node.action.ActionResult;
+import org.iot.dsa.node.action.DSAction;
 
 public class DBConnectionNode extends DSNode {
 
@@ -13,9 +16,25 @@ public class DBConnectionNode extends DSNode {
         put(db_name, params.get(JDBCv2Helpers.DB_NAME));
     }
 
+    private DSAction makeRemoveDatabaseAction() {
+        DSAction act = new DSAction() {
+            @Override
+            public ActionResult invoke(DSInfo info, ActionInvocation invocation) {
+                return ((DBConnectionNode) info.getParent()).removeDatabase();
+            }
+        };
+        return act;
+    }
+
+    private ActionResult removeDatabase() {
+        getParent().remove(getInfo());
+        return null;
+    }
+
     @Override
     protected void declareDefaults() {
         declareDefault(JDBCv2Helpers.DB_NAME, DSString.valueOf("No Name"));
+        declareDefault(JDBCv2Helpers.REMOVE, makeRemoveDatabaseAction());
     }
 
     @Override
--- src/main/java/org/iot/dsa/dslink/java/v2/jdbc/JDBCv2Helpers.java
+++ src/main/java/org/iot/dsa/dslink/java/v2/jdbc/JDBCv2Helpers.java
@@ -3,4 +3,5 @@ package org.iot.dsa.dslink.java.v2.jdbc;
 class JDBCv2Helpers {
     static final String ADD_DB = "Add DB";
     static final String DB_NAME = "Database Name";
+    static final String REMOVE = "Disconnect";
 }
