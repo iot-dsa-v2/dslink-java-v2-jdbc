343e298dbdeec268ae46edcb3a6346545dcd94b3
JurisPuchin <juris.puchin@gmail.com>
2017-10-09 14:40:58 -0700

    Table Column Typing Improvement

--- src/main/java/org/iot/dsa/dslink/java/v2/jdbc/JDBCClosedTable.java
+++ src/main/java/org/iot/dsa/dslink/java/v2/jdbc/JDBCClosedTable.java
@@ -7,9 +7,12 @@ import org.iot.dsa.node.action.ActionTable;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+
+import static java.awt.image.DataBuffer.TYPE_DOUBLE;
 
 public class JDBCClosedTable implements ActionTable {
 
@@ -25,7 +28,8 @@ public class JDBCClosedTable implements ActionTable {
 
         cols = new LinkedList<>();
         for (int i = 1; i <= columnCount; i++) {
-            cols.add(makeStrColumn(meta.getColumnName(i)));
+            DSValueType type = getColumnType(meta, i);
+            cols.add(makeColumn(meta.getColumnName(i), type));
         }
 
         rows = new LinkedList<>();
@@ -38,8 +42,56 @@ public class JDBCClosedTable implements ActionTable {
         }
     }
 
-    private static DSMap makeStrColumn(String name) {
-        return new DSMetadata().setName(name).setType(DSValueType.STRING).getMap();
+    private static DSMap makeColumn(String name, DSValueType type) {
+        return new DSMetadata().setName(name).setType(type).getMap();
+    }
+
+    private static DSValueType getColumnType(ResultSetMetaData meta, int idx) {
+        DSValueType ret = DSValueType.STRING;
+
+        try {
+            switch (meta.getColumnType(idx))
+            {
+                //null
+                case Types.NULL :
+                    ret = DSValueType.STRING;
+                    break;
+                //boolean
+                case Types.BOOLEAN :
+                    ret = DSValueType.BOOL;
+                    break;
+                //date
+                case Types.DATE :
+                case Types.TIMESTAMP :
+                    ret = DSValueType.STRING;
+                    break;
+                //double
+                case Types.DECIMAL :
+                case Types.DOUBLE :
+                case Types.FLOAT :
+                case Types.NUMERIC :
+                case Types.REAL :
+                    ret = DSValueType.NUMBER;
+                    break;
+                //duration
+                case Types.TIME :
+                    ret = DSValueType.STRING;
+                    break;
+                //long
+                case Types.BIT :
+                case Types.BIGINT :
+                case Types.INTEGER :
+                case Types.SMALLINT :
+                case Types.TINYINT :
+                    ret = DSValueType.NUMBER;
+                    break;
+                // String by default
+            }
+        } catch (SQLException e) {
+            //Not critical, will default to string in case of error.
+        }
+
+        return ret;
     }
 
     @Override
